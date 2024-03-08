51d6c5f15fde5a74ce4f8dc8acbebaf035c6af9b
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-03-07 23:03:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,85 @@
-const {users, movies} = require("./data");
-const cors = require('cors');
+//const {users, movies} = require("./data");
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
 const uuid = require('uuid');
-const PORT = 3030;
+
+let users = [
+    {
+      id:1,
+      fullname: 'John Doe',
+      email: 'johndoe@mail.com',
+      favMovies: [{
+        Title: 'Inception',
+        Director: 'Christopher Nolan',
+        Genre: 'Sci-Fi'
+      }]
+    },
+    {
+      id:2,
+      fullname: 'Jane Doe',
+      email: 'janedoe@mail.com',
+      favMovies: [{
+        Title: 'Inception',
+        Director: 'Christopher Nolan',
+        Genre: 'Sci-Fi'
+      }]
+    }
+  
+  ];
+  let movies = [
+      {
+        Title: 'Inception',
+        Director: 'Christopher Nolan',
+        Genre: 'Sci-Fi'
+      },
+      {
+        Title: 'Lord of the Rings',
+        Director: 'Peter Jackson',
+        Genre: 'Super-Heroes'
+      },
+      {
+        Title: 'The Matrix',
+        Director: 'Lana Wachowski',
+        Genre: 'Sci-fi'
+      },
+      {
+        Title: 'The Avengers',
+        Director: 'Anthony Russo',
+        Genre: 'Super-Heroes'
+      },
+      {
+        Title: 'The Silence Of The Lambs',
+        Director: 'Jonathan Demme',
+        Genre: 'Suspense-Thriller'
+      },
+      {
+        Title: 'Terminator',
+        Director: 'James Cameron',
+        Genre: 'Action'
+      },
+      {
+        Title: 'The Prestige',
+        Director: 'Christopher Nolan',
+        Genre: 'Suspense-Thriller'
+      },
+      {
+        Title: 'Shutter Island',
+        Director: 'Martin Scorsese',
+        Genre:'Suspense-Thriller'
+      },
+      {
+        Title: 'The Fugitive',
+        Director: 'Andrew Davis',
+        Genre: 'Suspense-Thriller'
+      },
+      {
+        Title: 'The Shawshank Redemption',
+        Director: 'Frank Darabont',
+        Genre: 'Action-thriller'
+      }
+    ];
 
 //Logging middleware
 app.use(morgan('common'));
@@ -13,7 +87,7 @@ app.use(express.json());
 
 //Get request for returning JSON movie data
 app.get('/movies', (req, res) => {
-    res.json(movies);
+    res.status(200).json(movies);
 });
 
 //Get request for returning personal message
@@ -31,58 +105,110 @@ app.use((err, req, res, next) => {
 })
 
 //Returning data a single movie
-app.get('/movies/title/:title', (req,res) => {
-    const movie = movies.find((m)=> m.title == req.params.title);
-    res.send(movie);
+app.get('/movies/:title', (req,res) => {
+    const {title} = req.params;
+    const movie= movies.find( movie => movie.Title ===title);
+    
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(400).send("no such movie")
+    }
 });
 
 //Returning data a single genre
-app.get('/movies/genre/:genre', (req,res) => {
-    res.send(movies_);
+app.get('/movies/genre/:genreName', (req,res) => {
+    const {genreName} = req.params;
+    const genre= movies.find( movie => movie.Genre.Name === genreName).Genre;
+    
+    if (genre) {
+        res.status(200).json(movies);
+    } else {
+        res.status(400).send("no such genre")
+    }
 });
 
 //Returning data of a director by name
-app.get('/movies/director/:director', (req,res) => {
-    const director = movies.filter((m)=> m.director == req.params.director);
-    res.send(director);
+app.get('/movies/director/:directorName', (req,res) => {
+    const {directorName} = req.params;
+    const director= movies.find( movie => movie.Director.Name === directorName).Director;
+    
+    if (director) {
+        res.status(200).json(movies);
+    } else {
+        res.status(400).send("no such director")
+    }
 });
 
 //Allow new users to register
-app.post('/users/register', (req,res) => {
-    users.push(req.body);
-    res.send('Registeration Successful');
-});
-app.get('/users', (req,res) => {
-    res.send(users);
+app.post('/users', (req,res) => {
+    const newUser = req.body;
+
+    if (newUser.name) {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).json(newUser)
+    } else {
+        res.status(400).send('user needs name')
+    }
 });
 
 //Allow Update user by username
 app.put('/users/update/:id', (req,res) => {
-    let userId = users.findIndex((u)=>u.id==req.params.id);
-    res.send(users.slice(userId,1, {...req.body}));
+    const { id }= req.params;
+    const updateUser= req.body;
+
+    let user = users.find(user => user.id == id);
+
+    if (user) {
+        user.name = updateUser.name;
+        res.status(200).json(user);
+    } else {
+        res.status(400).send ('no such user')
+    }
 });
  
 //Allow users to add a movie to their favorite movie list
-app.post('/favorite/add/:id', (req,res) => {
-    const user = users.find((u) => u.id ==req.params.id);
-    user.favMovies.push(req.body);
-    res.send(user);
+app.post('/users/:id/:movieTitle', (req,res) => {
+    const {id, movieTitle}= req.params;
+
+    let user = user.find( user => user.id == id);
+
+    if (user) {
+        user.favoriteMovies.push(movieTitle);
+        res.status(200).send('${movietitle} has been added to user ${id} array');
+    } else {
+        res.status(400).send('no such user')
+    }
 });
 
 //Allow users to remove movie from their favorite list
-app.delete('/favorite/delete/:id/:title', (req,res) => {
-    const user = users.find((u) => u.id ==req.params.id);
-    const favs = user.favMovies.filter((m)=>m.title !=req,params.title)
-    user.favMovies = [...favs];
-    res.send(user);
+app.delete('/users/:id/:movieTitle', (req,res) => {
+    const {id, movieTitle}= req.params;
+
+    let user = user.find( user => user.id == id);
+
+    if (user) {
+        user.favoriteMovies= user.favoriteMovie.filter( title => title !== movieTitle);
+        res.status(200).send('${movietitle} has been removed from user ${id} array');
+    } else {
+        res.status(400).send('no such user')
+    }
 });
 
 //Allow existing users to deregister
-app.delete('/users/deregister/:id', (req,res) => {
-    res.send(users.filter((m) => m.id !=req.params.id))
-});
+app.delete('/users/:id', (req,res) => {
+    const {id}= req.params;
 
-//Listening code
-app.listen(8080, () => {
-    console.log('Your app is listening on port 8080');
+    let user = user.find( user => user.id == id);
+
+    if (user) {
+        users = users.filter( user => user.id !== id);
+        res.json(users);
+        res.status(200).send('user ${id} has been deleted');
+    } else {
+        res.status(400).send('no such user')
+    }
 });
+//Listening code
+app.listen(8080, () => console.log('Your app is listening on port 8080'));
