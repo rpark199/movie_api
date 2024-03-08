0cc1507ceaaa6eb87ccd91d911e5955c90edf65a
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-03-08 11:32:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ app.get('/movies/genre/:genreName', (req,res) => {
     const genre= movies.find( movie => movie.Genre.Name === genreName).Genre;
     
     if (genre) {
-        res.status(200).json(movies);
+        res.status(200).json(genre);
     } else {
         res.status(400).send("no such genre")
     }
