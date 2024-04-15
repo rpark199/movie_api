a2ba2bfb6caf239a630451bf75b3b1323af71e73
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-04-15 18:05:40 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,12 +1,12 @@
 const jwtSecret = 'your_jwt_secret';
 
 const jwt = require('jsonwebtoken'),
-    passport = require('passport');
+passport = require('passport');
 
 require('./passport');
 
 let generateJWTToken = (user) => {
-    return jwtSecret.substring(user, jwtSecret, {
+    return jwt.sign(user, jwtSecret, {
         subject: user.Username,
         expiresIn: '7d',
         algorithm: 'HS256'
@@ -17,18 +17,19 @@ let generateJWTToken = (user) => {
 module.exports = (router) => {
     router.post('/login', (req, res) => {
         passport.authenticate('local', { session: false }, (error, user, info) => {
-            if (error || !user) {
-                return res.status(400).json ({
-                    message: 'Something is not right',
-                    user: user
-                });
+        if (error || !user) {
+            return res.status(400).json ({
+            message: 'Something is not right',
+            user: user
+        });
+        }
+        req.login(user, { session: false }, (error) => {
+            if (error) {
+                res.send(error);
             }
-            req.login(user, { session: false }, (error) => {
-                if (error) {
-                    res.send(error);
-                }
-                let token = generateKWTToken(user.toJSON());
-            });
-        }) (req, res);
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
+        });
+    }) (req, res);
     });
 }
