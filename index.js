9b0f09c087a4f890073cfa5b6e5a53715d19e4b7
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-04-18 20:28:10 -0400

    Added listening port

--- index.js
+++ index.js
@@ -224,4 +224,7 @@ app.use((err, req, res, next) => {
 });
 
 //Listening code
-app.listen(8080, () => console.log('Your app is listening on port 8080'));
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+  console.log('Your app is listening on port 8080'+ port)
+});
