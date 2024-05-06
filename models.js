19dfb60f3e4f3328dcb128df3d0c6c66669860f6
John Akhilomen <jonbasanti@gmail.com>
2024-05-06 16:37:26 -0500

    Update models.js

--- models.js
+++ models.js
@@ -1,40 +1,45 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
+
 let movieSchema = mongoose.Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    genre: {
-        name: String,
-        description: String
+    Title: {type: String, required: true},
+    Description: {type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String
     },
-    director: {
-        name: String,
-        bio: String
+    Director: {
+        Name: String,
+        Bio: String
     },
-    actors: [String],
-    imagePath: String,
-    featured: Boolean
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean
 });
 
 let userSchema = mongoose.Schema({
+    Name: {type: String},
     Username: {type: String, required: true},
-    Password: {type:String, required: true},
+    Password: {type: String, required: true},
     Email: {type: String, required: true},
     Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'movie' }]
-});
-
-userSchema.statics.hashPassword = (password) => {
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  });
+  
+  userSchema.statics.hashPassword = (password) => {
     return bcrypt.hashSync(password, 10);
-};
+  };
+  
+  userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+  };
 
-userSchema.methods.validatePassword = function(Password) {
-    return bcrypt.compareSync(Password, this.Password);
-};
+  
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
 
-let movie = mongoose.model('movie', movieSchema);
-let user = mongoose.model('user', userSchema);
+module.exports.Movie = Movie;
+module.exports.User = User;
 
-module.exports.movie = movie;
-module.exports.user = user;
+
